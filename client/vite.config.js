948fa5cee9917892172b6ca6ddae9c9c8ce6a4c3
theRTima <roschintimur@gmail.com>
2026-06-08 06:06:53 +0300

    modified:   client/nginx.conf 	modified:   client/vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
         target: 'ws://localhost:8080',
         ws: true,
       },
+      '/rooms': {
+        target: 'http://localhost:8080',
+      },
     },
   },
 })
